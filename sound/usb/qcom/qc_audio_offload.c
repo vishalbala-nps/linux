3f4fc8f9275274cf80be34b72f61e68bf16a2a59
Wesley Cheng <quic_wcheng@quicinc.com>
2025-02-18 16:47:52 -0800

    FROMLIST v35 ALSA: usb-audio: qcom: Don't allow USB offload path if PCM device is in use
    Add proper checks and updates to the USB substream once receiving a USB QMI
    stream enable request.  If the substream is already in use from the non
    offload path, reject the stream enable request.  In addition, update the
    USB substream opened parameter when enabling the offload path, so the
    non offload path can be blocked.
    
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>

--- sound/usb/qcom/qc_audio_offload.c
+++ sound/usb/qcom/qc_audio_offload.c
@@ -1576,12 +1576,17 @@ static void handle_uaudio_stream_req(struct qmi_handle *handle,
 		goto response;
 	}
 
+	mutex_lock(&chip->mutex);
 	if (req_msg->enable) {
-		if (info_idx < 0 || chip->system_suspend) {
+		if (info_idx < 0 || chip->system_suspend || subs->opened) {
 			ret = -EBUSY;
+			mutex_unlock(&chip->mutex);
+
 			goto response;
 		}
+		subs->opened = 1;
 	}
+	mutex_unlock(&chip->mutex);
 
 	if (req_msg->service_interval_valid) {
 		ret = get_data_interval_from_si(subs,
@@ -1603,6 +1608,11 @@ static void handle_uaudio_stream_req(struct qmi_handle *handle,
 		if (!ret)
 			ret = prepare_qmi_response(subs, req_msg, &resp,
 						   info_idx);
+		if (ret < 0) {
+			mutex_lock(&chip->mutex);
+			subs->opened = 0;
+			mutex_unlock(&chip->mutex);
+		}
 	} else {
 		info = &uadev[pcm_card_num].info[info_idx];
 		if (info->data_ep_pipe) {
@@ -1632,6 +1642,9 @@ static void handle_uaudio_stream_req(struct qmi_handle *handle,
 		}
 
 		disable_audio_stream(subs);
+		mutex_lock(&chip->mutex);
+		subs->opened = 0;
+		mutex_unlock(&chip->mutex);
 	}
 
 response:
